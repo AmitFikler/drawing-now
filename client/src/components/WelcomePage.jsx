0b3b5aa04c4765e4d408590860d665d57b85326d
AmitFikler <amitfikler@gmail.com>
2022-04-24 12:24:17 +0300

    ✨ Implement events(socket) in welcome page

--- client/src/components/WelcomePage.jsx
+++ client/src/components/WelcomePage.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+import { io } from "socket.io-client";
+
+
 function WelcomePage() {
+    const socket = io("http://localhost:3003");
+    const [name, setName] = useState("");
+    
+    socket.on("invalidName", ()=> {
+        console.log("Invalid name");
+    })
+
+    const handleSubmit = () => {
+        setName("");
+        socket.emit("playerJoin", name);
+    };
+
+    socket.on("wait", () => {
+        console.log("Waiting for other player");
+    });
+
+    socket.on("startGame",()=>{
+        console.log("Game started");
+    })
+
     return (
         <>
-        <h1>Welcome to DRAWING_NOW</h1>
-        
+            <h1>Welcome to DRAWING_NOW</h1>
+            <h4>Please Enter a username:</h4>
+            <input type={"text"}  placeholder="username.." onChange={(e)=>setName(e.target.value)} value={name}/>
+            <button onClick={handleSubmit}>Enter</button>
         </>
      );
 }
