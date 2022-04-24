b18e6f9281c11596c50d511f6fb42d03fe3d0da3
AmitFikler <amitfikler@gmail.com>
2022-04-24 15:56:36 +0300

    🛠 Added cors and "startdraw", "waitingroom" event

--- server/index.js
+++ server/index.js
@@ -1,32 +1,44 @@
 const express = require('express');
 const { createServer } = require('http');
+const cors = require('cors');
 const { Server } = require('socket.io');
 
 const app = express();
 const PORT = 3003;
 const httpServer = createServer(app);
-const io = new Server(httpServer);
-let ChosenWord = ''; //
+app.use(cors());
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+let ChosenWord = '';
+let drawing = '';
 const players = [];
 
 io.on('connection', (socket) => {
   socket.on('playerJoin', (name) => {
-    if (players.includes(name)) {
+    if (players.includes(name) || !name) {
       socket.emit('invalidName');
     } else {
-      players.push(name);
+      players.push({ name, id: socket.id });
     }
     if (players.length === 1) {
-      socket.emit('wait');
+      socket.emit('waitForAPlayer');
     }
 
     if (players.length === 2) {
-      socket.emit('startGame');
+      io.to(players[0].id).emit('startDraw');
+      io.to(players[1].id).emit('waitingRoom');
     }
   });
 
   socket.on('chooseAWord', (word) => {
     ChosenWord = word;
+  });
+
+  socket.on('sendDraw', (uri) => {
+    drawing = uri;
   });
 });
 
