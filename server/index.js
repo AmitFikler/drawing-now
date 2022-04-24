5e87f563555d2306937269a502e76c41b38688db
AmitFikler <amitfikler@gmail.com>
2022-04-24 11:21:59 +0300

    ✨ Added some events- socket io server

--- server/index.js
+++ server/index.js
@@ -7,8 +7,23 @@ const PORT = 3003;
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+const players = [];
+
 io.on('connection', (socket) => {
-  // ...
+  socket.on('playerJoin', ({ name }) => {
+    if (players.includes(name)) {
+      socket.emit('invalidName');
+    } else {
+      players.push(name);
+    }
+    if (players.length === 1) {
+      socket.emit('wait');
+    }
+
+    if (players.length === 2) {
+      socket.emit('startGame');
+    }
+  });
 });
 
 httpServer.listen(PORT, () => {
