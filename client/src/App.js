5140ac558ae81a75783abb4b0fec0b501e4c16cb
AmitFikler <amitfikler@gmail.com>
2022-04-24 13:52:19 +0300

    🩹 Provide socket from app component

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,26 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
 import './App.css';
 import ChooseWord from './components/ChooseWord';
+import Draw from './components/Draw';
+import { io } from 'socket.io-client';
+
+import { useState } from 'react';
 function App() {
+  const [word, setWord] = useState('');
+  const socket = io('http://localhost:3003');
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/choose" element={<ChooseWord />} />
-          {/* <Route path='/drew' element={} />
-          <Route path='/guess' element={} />
+          <Route path='/' element={<WelcomePage socket={socket} />} />
+          <Route
+            path='/choose'
+            element={
+              <ChooseWord word={word} setWord={setWord} socket={socket} />
+            }
+          />
+          <Route path='/draw' element={<Draw word={word} socket={socket} />} />
+          {/* <Route path='/guess' element={} />
           <Route path='/wait' element={} /> */}
         </Routes>
       </Router>
--- client/src/components/WelcomePage.jsx
+++ client/src/components/WelcomePage.jsx
@@ -1,36 +1,39 @@
-import { useState } from "react";
-import { io } from "socket.io-client";
+import { useState } from 'react';
 
+function WelcomePage({ socket }) {
+  const [name, setName] = useState('');
 
-function WelcomePage() {
-    const socket = io("http://localhost:3003");
-    const [name, setName] = useState("");
-    
-    socket.on("invalidName", ()=> {
-        console.log("Invalid name");
-    })
+  const handleSubmit = () => {
+    setName('');
+    socket.emit('playerJoin', name);
+  };
 
-    const handleSubmit = () => {
-        setName("");
-        socket.emit("playerJoin", name);
-    };
+  socket.on('invalidName', () => {
+    // if the name is invalid
+    console.log('Invalid name');
+  });
 
-    socket.on("wait", () => {
-        console.log("Waiting for other player");
-    });
+  socket.on('wait', () => {
+    console.log('Waiting for other player');
+  });
 
-    socket.on("startGame",()=>{
-        console.log("Game started");
-    })
+  socket.on('startGame', () => {
+    console.log('Game started');
+  });
 
-    return (
-        <>
-            <h1>Welcome to DRAWING_NOW</h1>
-            <h4>Please Enter a username:</h4>
-            <input type={"text"}  placeholder="username.." onChange={(e)=>setName(e.target.value)} value={name}/>
-            <button onClick={handleSubmit}>Enter</button>
-        </>
-     );
+  return (
+    <>
+      <h1>Welcome to DRAWING_NOW</h1>
+      <h4>Please Enter a username:</h4>
+      <input
+        type={'text'}
+        placeholder='username..'
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <button onClick={handleSubmit}>Enter</button>
+    </>
+  );
 }
 
 export default WelcomePage;
