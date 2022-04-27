58480eea415c783ae6e685bd06f9a2cff4d4440a
AmitFikler <amitfikler@gmail.com>
2022-04-27 09:50:26 +0300

    ✨ put it on welcomeroom and app

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,54 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from 'react-router-dom';
 import WelcomePage from './components/WelcomePage';
 import './App.css';
 import ChooseWord from './components/ChooseWord';
 import Draw from './components/Draw';
 import { io } from 'socket.io-client';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import WaitingRoom from './components/WaitingRoom';
 import GuessRoom from './components/GuessRoom';
 function App() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState({ word: '', score: 1 });
+  const [waiting, setWaiting] = useState(false);
+  const [hiddenWord, setHiddenWord] = useState('');
+  const [img, setImg] = useState('');
   const socket = io('http://localhost:3003');
+
   return (
-    <div className='App'>
-      <Router>
-        <Routes>
-          <Route path='/' element={<WelcomePage socket={socket} />} />
-          <Route
-            path='/choose'
-            element={
-              <ChooseWord word={word} setWord={setWord} socket={socket} />
-            }
-          />
-          <Route path='/draw' element={<Draw word={word} socket={socket} />} />
-          <Route path='/guess' element={<GuessRoom socket={socket} />} />
-          <Route
-            path='/waitingRoom'
-            element={<WaitingRoom socket={socket} />}
-          />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <WelcomePage
+              socket={socket}
+              waiting={waiting}
+              setImg={setImg}
+              setHiddenWord={setHiddenWord}
+              setWaiting={setWaiting}
+            />
+          }
+        />
+        <Route
+          path='/choose'
+          element={<ChooseWord word={word} setWord={setWord} socket={socket} />}
+        />
+        <Route path='/draw' element={<Draw word={word} socket={socket} />} />
+        <Route
+          path='/guess'
+          element={
+            <GuessRoom socket={socket} hiddenWord={hiddenWord} img={img} />
+          }
+        />
+        <Route path='/waitingRoom' element={<WaitingRoom socket={socket} />} />
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/components/WelcomePage.jsx
+++ client/src/components/WelcomePage.jsx
@@ -1,32 +1,43 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { io } from 'socket.io-client';
 
-function WelcomePage({ socket }) {
+function WelcomePage({ socket, waiting, setImg, setHiddenWord, setWaiting }) {
   const navigate = useNavigate();
   const [name, setName] = useState('');
-  const [waiting, setWaiting] = useState(false);
+  // const [waiting, setWaiting] = useState(false);
 
   const handleSubmit = () => {
     setName('');
-    socket.emit('playerJoin', name);
+    socket?.emit('playerJoin', { name });
   };
+  useEffect(() => {
+    if (socket) {
+      socket.on('invalidName', () => {
+        // if the name is invalid
+        console.log('Invalid name');
+      });
 
-  socket.on('invalidName', () => {
-    // if the name is invalid
-    console.log('Invalid name');
-  });
+      socket.on('waitForAPlayer', () => {
+        setWaiting(true);
+      });
 
-  socket.on('waitForAPlayer', () => {
-    setWaiting(true);
-  });
-
-  socket.on('startDraw', () => {
-    navigate('/choose');
-  });
-  socket.on('waitingRoom', () => {
-    console.log('wait');
-    navigate('/waitingRoom');
-  });
+      socket.on('startDraw', () => {
+        navigate('/choose');
+      });
+      socket.on('waitingRoom', () => {
+        navigate('/waitingRoom');
+      });
+      socket.on('startGuess', (param) => {
+        console.log(param);
+        navigate('/guess');
+      });
+      socket.on('getDrawFromServer', ({ drawingUri, chosenWord }) => {
+        setImg(drawingUri);
+        setHiddenWord(chosenWord);
+      });
+    }
+  }, [socket]);
 
   return (
     <>
